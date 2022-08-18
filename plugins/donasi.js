4ff24978a2fa26a972cd96013875b9b36555a992
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-08-18 07:26:59 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,11 +2,11 @@ const qrku = "https://i.ibb.co/1LF6QHQ/pangeran.jpg"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • MyXL [087832147584]
-│ • Dana  [087832147584]
+│ • MyXL [+62 819-5929-3465]
+│ • Gopay  [+62 812-7367-7810]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? wa.me/+6285929970260
+│ > Ingin donasi? wa.me/6281273677810
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
